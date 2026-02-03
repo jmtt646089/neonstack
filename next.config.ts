3cb8ab981f1c382d6173b7ca430378a188a4c6cb
jmtt646089 <jadeseala@gmail.com>
2026-02-04 12:49:00 +1300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,16 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  
   devIndicators: {
     position: 'top-right',
   },
+
+  typescript: {
+    ignoreBuildErrors: false,
+    tsconfigPath: 'tsconfig.json',
+  },
+  
 };
 
 export default nextConfig;
