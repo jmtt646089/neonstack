07387327e548ff78d1d032fbf0befdb37aa929eb
jmtt646089 <jadeseala@gmail.com>
2026-02-04 10:30:42 +1300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { StackProvider, StackTheme } from '@stackframe/stack';
 import { SidebarProvider } from '../components/ui/sidebar';
 import { stackServerApp } from '../stack';
 //import { Geist, Geist_Mono } from 'next/font/google';
-import { Inter } from 'next/font/google';
+//import { Inter } from 'next/font/google';
 import './globals.css';
 import { appConfig } from '../config/app';
 
@@ -18,13 +18,15 @@ const geistMono = Geist_Mono({
   subsets: ['latin'],
 });
 
-        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}> 
-*/
-
 const inter = Inter({
   subsets: ['latin'],
   display: 'swap', // optional, but recommended
 });
+    <html lang="en" className = {inter.className}>
+
+        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}> 
+*/
+
 
 
 export const metadata: Metadata = {
@@ -38,7 +40,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className = {inter.className}>
+    <html lang="en" >
       <body >
 
         <StackProvider app={stackServerApp}>
