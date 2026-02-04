0351aa95b454d3ac9bd72fec7dc132a35746bfc8
jmtt646089 <jadeseala@gmail.com>
2026-02-04 13:03:39 +1300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,15 @@
 import type { NextConfig } from 'next';
 
+console.log("Check if next.config.ts is read and executed. -------------1---------------- JM ");
+
 const nextConfig: NextConfig = {
   
   devIndicators: {
     position: 'top-right',
   },
 
+  console.log("Check if next.config.ts is read and executed. ------------2----------------- JM ");
+  
   typescript: {
     ignoreBuildErrors: false,
     tsconfigPath: 'tsconfig.json',
