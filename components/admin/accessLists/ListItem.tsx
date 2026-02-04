dd3360e0fe65d61f79e3b8266851ab3f9afcc70d
jmtt646089 <jadeseala@gmail.com>
2026-02-04 14:14:39 +1300

    Update ListItem.tsx

--- components/admin/accessLists/ListItem.tsx
+++ components/admin/accessLists/ListItem.tsx
@@ -3,7 +3,7 @@
 import { useTransition } from 'react';
 import { toast } from 'sonner';
 import { XIcon } from 'lucide-react';
-import { Button } from '@/components/ui/button';
+import { Button } from '../../../components/ui/button';
 
 interface ListItemProps {
   item: {
